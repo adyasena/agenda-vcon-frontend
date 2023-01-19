2e1b4675fcf054d94e85a915944df1a9e15a8fac
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-19 09:57:27 +0700

    add home page

---
+++ src/page/homepage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../components/navbar";
+import Home from "../components/home";
+import Agenda from "../components/agenda";
+
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Agenda />
+    </>
+  )
+}
+
+export default Homepage ;
