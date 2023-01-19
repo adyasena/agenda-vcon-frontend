d722a93b433314ef9af77a853e41249bea5cefc3
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-19 09:57:01 +0700

    add home component

---
+++ src/components/home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url("../src/assets/home.jpg")`}}>
+      <div className="w-full h-screen bg-black bg-opacity-50">
+        <div className="flex flex-col">
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
