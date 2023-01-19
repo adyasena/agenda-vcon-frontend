f4e6a1bd43415584b62b2b5417073813bd49cff8
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-19 09:55:32 +0700

    add custom font and colors class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,25 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    colors: {
+      'blue-primary': '#00458E',
+      'blue-light': '#0093DD',
+      'blue-dark': '#28166F',
+      'black': '#151515',
+      'white': '#F3F3F3'
+    },
     extend: {},
+    screens: {
+      xm: "350px",
+      sm: "640px",
+      md: "913px",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
   plugins: [],
 }
