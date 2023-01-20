423d42b3e410663d1d8ffe1e2b2f6641c4806f8a
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-20 08:12:16 +0700

    update agenda content

--- src/components/agenda.jsx
+++ src/components/agenda.jsx
@@ -3,7 +3,21 @@ import React from "react";
 const Agenda = () => {
   return (
     <div className="w-full h-screen bg-white">
+      <div className="pt-20">
+        <div className="text-black text-center text-2xl font-poppins font-semibold">
+          Agenda Hari Ini
+        </div>
+        <div className="pt-10">
+          <table className="table">
+            <thead>
+              <tr>
+                
+              </tr>
+            </thead>
 
+          </table>
+        </div>
+      </div>
     </div>
   )
 }
