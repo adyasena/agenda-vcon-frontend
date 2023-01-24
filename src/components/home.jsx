244445a05ecc3702594fe3d74385dd5d2fd12709
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-24 08:16:14 +0700

    update home styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { useFetch } from "../utils/hooks/useFetch";
+import React, { useEffect, useState } from "react";
+import BgHome from "../assets/home.jpg";
+import LogoKominfo from "../assets/logo_kominfo.png";
+import LogoPurworejo from "../assets/logo_purworejo.png";
 
 const Home = () => {
   const scrollToBottom = () => {
@@ -9,16 +11,14 @@ const Home = () => {
     });
   };
 
-  const {error, isLoading, data: agendaData} = useFetch("/agenda");
-
   return (
-    <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url("../src/assets/home.jpg")`}}>
+    <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${BgHome})`}}>
       <div className="w-full h-screen bg-black bg-opacity-60">
         <div className="flex flex-col gap-10 h-full container mx-auto items-center font-poppins text-white">
           <div className="flex flex-row justify-center items-center mt-32 h-24">
-            <img src="../src/assets/logo_kominfo.png" alt="logo kominfo" className="h-full w-auto scale-95" />
+            <img src={LogoKominfo} alt="logo kominfo" className="h-full w-auto scale-95" />
             <box className="h-full mx-6 p-[0.07rem] rounded-sm bg-white"></box>
-            <img src="../src/assets/logo_purworejo.png" alt="lambang purworejo" className="h-full w-auto" />
+            <img src={LogoPurworejo} alt="lambang purworejo" className="h-full w-auto" />
           </div>
           <div className="font-semibold text-3xl text-center leading-normal">
             Agenda Penggunaan Fasilitas VCON<br />
@@ -29,12 +29,13 @@ const Home = () => {
             <button className="bg-blue-light text-white font-normal py-2 px-6 rounded-full transform duration-300 ease hover:bg-white hover:text-black" onClick={scrollToBottom}>Lihat Agenda</button>
             
           </div>
+          <div>
+          
+          </div>
         </div>
       </div>
     </div>
   )
 }
-/* {agendaData?.data?.agenda.map((agenda) => (
-  <p>{agenda?.tanggal}</p>
-))} */
+
 export default Home;
