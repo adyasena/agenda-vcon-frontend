30e54b5691f4a093a43198f77c5c640e7e49c1fb
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-25 13:58:35 +0700

    add route to loginpage and adminpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./page/homepage";
+import Loginpage from "./page/loginpage";
+import Adminpage from "./page/adminpage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/vcon-admin" element={<Loginpage />} />
+        <Route path="/vcon-login" element={<Adminpage />} />
       </Routes>
     </>
   );
