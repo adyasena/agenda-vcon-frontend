34ee46f243faaaab165eed0e8e3f60518bd540e4
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-25 09:30:00 +0700

    add home and navbar responsive styling

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,18 +14,18 @@ const Home = () => {
   return (
     <div className="w-full h-screen bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${BgHome})`}}>
       <div className="w-full h-screen bg-black bg-opacity-60">
-        <div className="flex flex-col gap-10 h-full container mx-auto items-center font-poppins text-white">
+        <div className="flex flex-col lg:gap-10 gap-14 h-full container mx-auto lg:py-8 py-2 items-center font-poppins text-white">
           <div className="flex flex-row justify-center items-center mt-32 h-24">
-            <img src={LogoKominfo} alt="logo kominfo" className="h-full w-auto scale-95" />
-            <box className="h-full mx-6 p-[0.07rem] rounded-sm bg-white"></box>
-            <img src={LogoPurworejo} alt="lambang purworejo" className="h-full w-auto" />
+            <img src={LogoKominfo} alt="logo kominfo" className="lg:h-full h-16 w-auto scale-95" />
+            <div className="lg:h-full h-16 mx-6 p-[0.07rem] rounded-sm bg-white"></div>
+            <img src={LogoPurworejo} alt="lambang purworejo" className="lg:h-full h-16 w-auto" />
           </div>
-          <div className="font-semibold text-3xl text-center leading-normal">
+          <div className="font-semibold lg:text-3xl text-2xl text-center mt-[-10px] leading-normal">
             Agenda Penggunaan Fasilitas VCON<br />
             Dinas Komunikasi, Informatika, Statistik dan Persandian<br />
             Kabupaten Purworejo
           </div>
-          <div className="mt-10">
+          <div className="lg:mt-10">
             <button className="bg-blue-light text-white font-normal py-2 px-6 rounded-full transform duration-300 ease hover:bg-white hover:text-black" onClick={scrollToBottom}>
               Lihat Agenda
             </button>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,13 +18,13 @@ const Navbar = () => {
     <div className="navbar">
       <div className={"navbar bg-gradient-to-l from-blue-light to-blue-primary " + (scroll ? "opacity-100" : "h-24 opacity-0")}>
       </div>
-      <div className={"z-[1] font-poppins sticky flex flex-row container mx-auto px-12 text-center items-center text-lg font-semibold " +
+      <div className={"z-[1] font-poppins sticky flex flex-row container mx-auto lg:px-12 text-center items-center text-lg font-semibold " +
         "justify-between transform duration-300 ease overflow-hidden " + (scroll ? "h-16" : "h-24")}>
         <div className="flex flex-row justify-start items-center text-white h-full">
           <img src={LogoKominfo} alt="logo kominfo" className={"m-2 transform duration-300 ease " + (scroll ? "w-8" : "w-0 mr-[-4px]")}/>
           <p className="transform">Agenda VCON</p> 
         </div>
-          <button className={"text-base border-2 font-normal py-2 px-6 rounded-full transform duration-300 ease "
+          <button className={"text-base border-2 font-normal py-2 lg:px-6 px-4 rounded-full transform duration-300 ease "
             + (scroll ? "bg-white text-blue-light border-white hover:bg-blue-light hover:text-white" : "text-white border-blue-light hover:bg-blue-light")}>
             Hubungi Kami
           </button> 
