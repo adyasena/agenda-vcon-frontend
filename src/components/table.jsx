57b0b331bf83e94b273e33c1d5ff6d7bdb26030b
adyasena <adyasena01@mail.ugm.ac.id>
2023-01-27 15:32:03 +0700

    add input agenda modal

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useTable, useFilters, useGlobalFilter } from "react-table";
-import Moment from "moment/moment";
 
 export default function Table({ columns, data }) {
   const {
@@ -14,57 +13,36 @@ export default function Table({ columns, data }) {
     prepareRow 
   } = useTable({ columns, data }, useFilters, useGlobalFilter);
 
-  const [filterTahun, setFilterTahun] = useState("");
-  const [filterInput, setFilterInput] = useState("");
-  const [displayTanggal, setDisplayTanggal] = useState("");
-
-  const handleFilterTanggal = e => {
-    const value = Moment(e.target.value)
-    .local('id')
-    .format("DD MMMM YYYY");
-    console.log(value)
-    setFilter("Tanggal", value);
-    setFilterInput(value);
-  };
   const handleFilterTahun = e => {
     const value = e.target.value;
-    console.log(e.target.value)
     setFilter("Tanggal", value);
-    setFilterTahun(value);
-  };
-
-  const handleDisplayTanggal = e => {
-    const value = Moment(e.target.value)
-    .local('id')
-    .format("yyyy-MM-DD");
-    setDisplayTanggal(value);
-    handleFilterTanggal(e);
   };
 
   const { globalFilter } = state;
 
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="text-black">
-      <div className="flex flex-row justify-start gap-2">
-        <input
-          type="text"
-          placeholder="Cari" 
-          className="py-2 px-3 w-40 rounded-md border border-black bg-white mb-3 focus:outline-blue-light"
-          value={globalFilter || ''}
-          onChange={(e) => setGlobalFilter(e.target.value)}
-        />
-        <input
-          type="date"
-          placeholder="Cari" 
-          className="py-2 px-3 w-40 rounded-md border border-black bg-white mb-3 focus:outline-blue-light"
-          value={displayTanggal}
-          onChange={handleDisplayTanggal}
-        />
-        <select defaultValue={2023} className="px-3 w-40 rounded-md border border-black bg-white mb-3 outline-none">
-          <option value="">Semua Tahun</option>
-          <option value="2022">2022</option>
-          <option value="2023">2023</option>
-        </select>
+      <div className="flex flex-row justify-between gap-2 align-center">
+        <div className="flex flex-row justify-start gap-2 align-center">
+          <input
+            type="text"
+            placeholder="Cari" 
+            className="py-2 px-3 w-40 rounded-md border border-black bg-white mb-3 focus:outline-blue-light"
+            value={globalFilter || ''}
+            onChange={(e) => setGlobalFilter(e.target.value)}
+          />
+          <select defaultValue={2023} onChange={handleFilterTahun} className="px-3 w-40 rounded-md border border-black bg-white mb-3 outline-none">
+            <option value="">Semua Tahun</option>
+            <option value="2022">2022</option>
+            <option value="2023">2023</option>
+          </select>
+        </div>
+        <button className="text-base py-2 px-4 mb-3 rounded-md hover:bg-blue-primary bg-blue-light text-white"
+          onClick={() => setShowModal(true)}>
+            Tambah Agenda
+        </button> 
       </div>
       <table {...getTableProps()} className="w-full table-fixed">
         <thead>
@@ -103,6 +81,48 @@ export default function Table({ columns, data }) {
           Tidak ada agenda.
         </div>
       }
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl font-roboto">
+              <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <div className="text-3xl font-semibold">
+                    Tambah Agenda
+                  </div>
+                  <button className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none" onClick={() => setShowModal(false)}>
+                    x
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    I always felt like I could do anything. That’s the main
+                    thing people are controlled by! Thoughts- their perception
+                    of themselves! They're slowed down by their perception of
+                    themselves. If you're taught you can’t do anything, you
+                    won’t do anything. I was taught I could do everything.
+                  </p>
+                </div>
+                <div className="flex items-center justify-end p-6 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                      Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                      Save Changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-50 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
     </div>
   );
 }
