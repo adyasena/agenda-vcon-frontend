847ec9d643fe9056b98d710b5f417804df0fa3d0
adyasena <adyasena01@mail.ugm.ac.id>
2023-02-02 11:06:16 +0700

    update login styling

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -44,14 +44,14 @@ const Login = () => {
         <div className="container m-auto rounded-xl bg-white flex flex-col gap-5 items-center w-1/3 h-auto p-12 justify-center font-roboto">
           <div className="flex flex-col justify-start gap-1 w-full">
             <p>Username</p>
-            <input required onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="Enter username" className="py-2 px-5 rounded-full border border-black bg-white focus:outline-blue-light" />
+            <input required onChange={(e) => setUsername(e.target.value)} value={username} type="text" placeholder="Enter username" className="py-2 px-5 rounded-md border border-black bg-white focus:outline-blue-light" />
           </div>
           <div className="flex flex-col justify-start gap-1 w-full">
             <p>Password</p>
-            <input value={password} onChange={(e) => setPassword(e.target.value)} type="text" placeholder="Enter password" className="py-2 px-5 rounded-full border border-black bg-white focus:outline-blue-light" />
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter password" className="py-2 px-5 rounded-md border border-black bg-white focus:outline-blue-light" />
           </div>
           <div className="lg:mt-5">
-            <button onClick={signInHandler} className="bg-blue-light text-white font-normal py-2 px-6 rounded-full transform duration-300 ease hover:bg-blue-primary hover:text-white" >
+            <button onClick={signInHandler} className="bg-blue-light text-white font-normal py-2 px-6 rounded-md transform duration-300 ease hover:bg-blue-primary hover:text-white" >
               Login Admin
             </button>
           </div>
